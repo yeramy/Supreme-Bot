39b7fb1e2d7f16c91865f78322dd25bccee57daa
Yeram <yeramy11@naver.com>
2020-09-02 16:36:03 -0700

    trash delete

--- aa.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from config import keys
-from webdriver_manager.chrome import ChromeDriverManager
-import time
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
