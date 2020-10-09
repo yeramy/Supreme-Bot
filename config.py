8cabf2dae25438c922c58841fb48ec70f64d70ca
Yeram's laptop <yeramy11@naver.com>
2020-10-08 17:44:35 -0700

    config.py added to test

---
+++ config.py
@@ -0,0 +1,20 @@
+keys = {
+        "name": "John Fisher",
+        "email": "email@gmail.com",
+        "phone_number": "6789998212",
+        "address": "Forest Hills Dr.",
+        "apt": "Timbuktu",
+        "zip": "91601",
+        "city": "666",
+        "cvv": "123",
+        "cc": "Timbuktu",
+        "zip": "90033",
+        "em": "01",
+        "ey": "2020"
+}
+productn = {
+    "category":"",
+    "keyword":"",
+    "color":"",
+    #"size":"this can be configured in main source code"
+}
