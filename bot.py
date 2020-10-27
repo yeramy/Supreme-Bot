fa745e36ede4746b8a2a6399132347d0011e4144
Yeram's laptop <yeramy11@naver.com>
2020-10-26 18:12:27 -0700

    Change few codes according to file format change

--- bot.py
+++ bot.py
@@ -1,21 +1,29 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from config import keys
 from config import product
+# from xml.dom import minidom
+import xml.etree.ElementTree as ET
 import time
 
-
 def order():
+    tree = ET.parse('savedbinfo.xml')
+    root = tree.getroot()
+
     # wait for checkout button element to load
     time.sleep(.3)
     # fill out checkout screen fields
-    driver.find_element_by_id('order_billing_name').send_keys(keys['name'])
-    driver.find_element_by_id('order_email').send_keys(keys['email'])
-    driver.find_element_by_id('order_tel').send_keys(keys['phone_number'])
-    driver.find_element_by_id('bo').send_keys(keys['address'])
-    driver.find_element_by_id('oba3').send_keys(keys['apt'])
-    driver.find_element_by_id('order_billing_zip').send_keys(keys['zip'])
-    driver.find_element_by_id('order_billing_city').send_keys(keys['city'])
+    driver.find_element_by_id('order_billing_name').send_keys(root[0].text)
+    driver.find_element_by_id('order_email').send_keys(root[1].text)
+    driver.find_element_by_id('order_tel').send_keys(root[2].text)
+    driver.find_element_by_id('bo').send_keys(root[3].text)
+    driver.find_element_by_id('oba3').send_keys(root[4].text)
+    driver.find_element_by_id('order_billing_zip').send_keys(root[5].text)
+    driver.find_element_by_id('order_billing_city').send_keys(root[6].text)
+    driver.find_element_by_id('order_billing_state').send_keys(root[7].text)
+    driver.find_element_by_id('order[billing_country]').send_key(root[8].text)
+
+
+
     driver.find_element_by_id('orcer').send_keys(keys['cvv'])
     driver.find_element_by_id('rnsnckrn').send_keys(keys['cc'])
     driver.find_element_by_name('credit_card[month]').send_keys(keys['em'])
@@ -28,7 +36,7 @@ def order():
 
 
 if __name__ == '__main__':
-    # load chrome
+    # load chrome and parse .xml file
     driver = webdriver.Chrome(ChromeDriverManager().install())
 
     # Goto Surpeme url
--- config.py
+++ config.py
@@ -1,19 +1,5 @@
-keys = {
-        "name": "John Fisher",
-        "email": "email@gmail.com",
-        "phone_number": "6789998212",
-        "address": "Forest Hills Dr.",
-        "apt": "Timbuktu",
-        "zip": "91601",
-        "city": "666",
-        "cvv": "123",
-        "cc": "Timbuktu",
-        "zip": "90033",
-        "em": "01",
-        "ey": "2020"
-}
 product = {
     "category":"jackets",
-    "keyword":"Racing",
+    "keyword":"King",
     "color":"Black"
 }
