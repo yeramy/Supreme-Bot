88165da628d3be3aadb73e882eb2448b97124fac
Yeram's laptop <yeramy11@naver.com>
2020-10-27 08:13:59 -0700

    unsaved files

--- bot.py
+++ bot.py
@@ -1,31 +1,37 @@
 from selenium import webdriver
-
 from webdriver_manager.chrome import ChromeDriverManager
-from config import keys
 from config import product
 import time
+import xml.etree.ElementTree as ET
 
 
 def order():
     # wait for checkout button element to load
     time.sleep(.3)
+    tree = ET.parse('savedinfo.xml')
+    root = tree.getroot()
+
+
     # fill out checkout screen fields
-    driver.find_element_by_id('order_billing_name').send_keys(keys['name'])
-    driver.find_element_by_id('order_email').send_keys(keys['email'])
-    driver.find_element_by_id('order_tel').send_keys(keys['phone_number'])
-    driver.find_element_by_id('bo').send_keys(keys['address'])
-    driver.find_element_by_id('oba3').send_keys(keys['apt'])
-    driver.find_element_by_id('order_billing_zip').send_keys(keys['zip'])
-    driver.find_element_by_id('order_billing_city').send_keys(keys['city'])
-    driver.find_element_by_id('orcer').send_keys(keys['cvv'])
-    driver.find_element_by_id('rnsnckrn').send_keys(keys['cc'])
-    driver.find_element_by_name('credit_card[month]').send_keys(keys['em'])
-    driver.find_element_by_name('credit_card[year]').send_keys(keys['ey'])
+    driver.find_element_by_id('order_billing_name').send_keys(root[0])
+    driver.find_element_by_id('order_email').send_keys(root[1])
+    driver.find_element_by_id('order_tel').send_keys(root[2])
+    driver.find_element_by_id('bo').send_keys(root[3])
+    driver.find_element_by_id('oba3').send_keys(root[4])
+    driver.find_element_by_id('order_billing_zip').send_keys(root[5])
+    driver.find_element_by_id('order_billing_city').send_keys(root[6])
+
+
+
+    #driver.find_element_by_id('orcer').send_keys(root[0])
+    #driver.find_element_by_id('rnsnckrn').send_keys(root[0])
+    #driver.find_element_by_name('credit_card[month]').send_keys(root[0])
+    #driver.find_element_by_name('credit_card[year]').send_keys(keys['ey'])
 
     # Process payment click
-    time.sleep(.5)
-    driver.find_element_by_xpath("icheckbox_minimal").click()
-    driver.find_element_by_name('commit').click()
+    # time.sleep(.5)
+    # driver.find_element_by_xpath("icheckbox_minimal").click()
+    # driver.find_element_by_name('commit').click()
 
 
 if __name__ == '__main__':
@@ -36,7 +42,7 @@ if __name__ == '__main__':
     driver.get('https://www.supremenewyork.com/shop/all')
 
     # Find items
-
+    product = product
     # Category selector
     category = driver.find_element_by_link_text(product['category'])
     category.click()
--- config.py
+++ config.py
@@ -1,20 +1,6 @@
-keys = {
-        "name": "John Fisher",
-        "email": "email@gmail.com",
-        "phone_number": "6789998212",
-        "address": "Forest Hills Dr.",
-        "apt": "Timbuktu",
-        "zip": "91601",
-        "city": "666",
-        "cvv": "123",
-        "cc": "Timbuktu",
-        "zip": "90033",
-        "em": "01",
-        "ey": "2020"
-}
-productn = {
-    "category":"",
-    "keyword":"",
-    "color":"",
+product = {
+    "category":"jackets",
+    "keyword":"King",
+    "color":"Black",
     #"size":"this can be configured in main source code"
 }
